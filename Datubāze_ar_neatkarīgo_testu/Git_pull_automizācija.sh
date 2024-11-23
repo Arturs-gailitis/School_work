bae7d5538dfbdb03549981cf3a29800ff22b2f73
Arturs-gailitis <artursgailitis2003@gmail.com>
2024-11-23 15:50:52 +0200

    Izveidots skrips, kas automizēs git pull

---
+++ Datubāze_ar_neatkarīgo_testu/Git_pull_automizācija.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+REPO_PATH="OneDrive/Desktop/Work/School_work/Datubāze_ar_neatkarīgo_testu"
+
+if [ -d "$REPO_PATH"]; then
+
+    echo "Repozitorijs ir atrasts: $REPO_PATH"
+
+    cd "$REPO_PATH" || {echo "Neizdevās atrast direktoriju."; exit 1; }
+
+    echo "Atjaunina repozitoriju..."
+    git pull || {echo "Git pull neizdevās."; exit 1; }
+
+    echo "Izdevās git pull."
+
+else
+
+    echo "Direktorija neeksistē."
+
+fi
