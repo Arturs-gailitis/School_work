8d205b615697866cca2c6af3a7b8f55f442ff031
Arturs-gailitis <artursgailitis2003@gmail.com>
2024-11-24 12:27:17 +0200

    Ievietoju komentārus Datubāzu_vērtības.py

--- Datubāze_ar_neatkarīgo_testu/Datubāzu_vērtības.py
+++ Datubāze_ar_neatkarīgo_testu/Datubāzu_vērtības.py
@@ -5,8 +5,11 @@ def iegūt_kolonnas():
     connecting = Savienošana()
     cursor = connecting[1]
 
+    # Iegūst tabulas 'suni' kolonnu nosaukumus, izņemot 'ID'.
+
     cursor.execute("PRAGMA table_info(suni)")
 
+    # Izveido sarakstu ar kolonnu nosaukumiem
     columns = [row[1] for row in cursor.fetchall() if row[1] != "ID"]
 
     return columns
@@ -17,6 +20,9 @@ def ielikt_vērtības(column_names, values):
     cursor = conecting[1]
     connection = conecting[0]
 
+    # Ievieto vērtības tabulā 'suni', norādot kolonnas un vērtības.
+
+    # Sagatavo vaicājumu ar jautājuma zīmēm vērtībām
     question_marks = ", ".join(['?'] * len(column_names))
 
     query = f"INSERT INTO suni ({", ".join(column_names)}) VALUES ({question_marks})"
